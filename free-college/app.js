b4ff31f8e4b4b65bd494ab6b58eca1443d24abc5
AlexsanderDiego <alexdiegoat@gmail.com>
2023-11-25 00:12:43 -0300

    update RouterApp

--- free-college/app.js
+++ free-college/app.js
@@ -3,9 +3,9 @@ const app = express()
 const alunosRouter = require('./routes/alunos')
 const salasRouter = require('./routes/salas')
 const departamentosRouter = require('./routes/departamentos')
-// const professoresRouter = require('./routes/professores')
-// const cursosRouter = require('./routes/cursos')
-// const matriculasRouter = require('./routes/matriculas')
+const professoresRouter = require('./routes/professores')
+const cursosRouter = require('./routes/cursos')
+const matriculasRouter = require('./routes/matriculas')
 const port = 3000
 
 app.use(express.json());
@@ -23,14 +23,14 @@ app.use('/salas', salasRouter);
 // Prefixo da rota de departamentos /departamentos
 app.use('/departamentos', departamentosRouter);
 
-// // Prefixo da rota de professores /professores
-// app.use('/professores', professoresRouter);
+// Prefixo da rota de professores /professores
+app.use('/professores', professoresRouter);
 
-// // Prefixo da rota de matriculas /matriculas
-// app.use('/matriculas', matriculasRouter);
+// Prefixo da rota de matriculas /matriculas
+app.use('/matriculas', matriculasRouter);
 
-// // Prefixo da rota de cursos /cursos
-// app.use('/cursos', cursosRouter);
+// Prefixo da rota de cursos /cursos
+app.use('/cursos', cursosRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
